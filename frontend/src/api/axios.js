f8e2c50ab2687fb2665e3132c98ef9ff773699e6
Tushar <tusharsingla111205@gmail.com>
2026-09-12 11:23:50 +0530

    add backend url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://job-portal-applicant-tracking-system.onrender.com/api"
 });
 
 API.interceptors.request.use((req) => {
